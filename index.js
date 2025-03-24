18655c3db49e51501972a7d77b726270f3628f1b
Marciah <marciah.machoka@student.moringaschool.com>
2025-03-24 21:22:15 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee= {name:'Marciah', streetaddress:'Langatal7'}
+
+// Function to non-destructively update employee with a new key-value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] =value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key){
+    const newEmployee ={...employee};
+    delete newEmployee[key];
+    return newEmployee
+  }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
+
